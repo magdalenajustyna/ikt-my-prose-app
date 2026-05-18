d92bde5f9f7f43916c92b0d31d58967687ed6330
Magdalena Ucar <magdalena.ucar@student.htw-berlin.de>
2026-05-18 16:27:29 +0200

    App runs now

--- src/main/java/de/htw_berlin/fb4/magda/App.java
+++ src/main/java/de/htw_berlin/fb4/magda/App.java
@@ -1,13 +1,16 @@
 package de.htw_berlin.fb4.magda;
 
-/**
- * Hello world!
- *
- */
+import de.htw_berlin.fb4.ossd.prose.ProseBuilder;
+
 public class App 
 {
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        ProseBuilder builder = new ProseBuilder();
+        builder.register(new SentenceStart());
+        builder.register(new SentenceEnd());
+        System.out.println(builder.get());
+     
     }
 }
