ca1c8b8cb465954105a13fca6f5729b6dbc0ff5b
Magdalena Ucar <magdalena.ucar@student.htw-berlin.de>
2026-05-18 16:30:04 +0200

    Create two classes that implements Sentence

---
+++ src/main/java/de/htw_berlin/fb4/magda/SentenceEnd.java
@@ -0,0 +1,12 @@
+package de.htw_berlin.fb4.magda;
+
+import de.htw_berlin.fb4.ossd.prose.Sentence;
+
+public class SentenceEnd implements Sentence{
+
+    @Override
+    public String get(){
+        return "Magda!";
+    }
+    
+}
---
+++ src/main/java/de/htw_berlin/fb4/magda/SentenceStart.java
@@ -0,0 +1,11 @@
+package de.htw_berlin.fb4.magda;
+
+import de.htw_berlin.fb4.ossd.prose.Sentence;
+
+public class SentenceStart implements Sentence {
+    @Override
+    public String get(){
+        return "Ich heiße ...";
+    }
+    
+}
